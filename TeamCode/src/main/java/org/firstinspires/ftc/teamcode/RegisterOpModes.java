1f7856945809a3889fc3f633693aa1c84c295bea
Mordecai Man <danehalle@gmail.com>
2018-02-05 12:26:08 -0500

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RegisterOpModes.java
@@ -0,0 +1,30 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
+import com.qualcomm.robotcore.eventloop.opmode.OpModeRegistrar;
+
+//import org.firstinspires.ftc.robotcontroller.external.samples.TestColor;
+//import org.firstinspires.ftc.teamcode.Crap.K9botTeleopTank_Linear;
+//import org.firstinspires.ftc.teamcode.Crap.SensorMRColor;
+
+
+public class RegisterOpModes {
+
+    @OpModeRegistrar
+    public static void registerMyOpModes(OpModeManager manager) {
+        //manager.register("TestWheels",TestWheels.class);
+        manager.register("Autonomous Red", Auto_Mechanum_Red.class);
+        manager.register("Autonomous Blue", Auto_Mechanum_Blue.class);
+        //manager.register("TestColor", TestColor.class);
+        //manager.register("Driver Controlled", Driver_Mechanum_TankMode.class);
+        //manager.register("Single Driver", Single.class);
+        manager.register("Dual Driver", Dual.class);
+        //manager.register("test",TestWheels.class);
+        manager.register("Wheels", New_Wheels.class);
+
+        //manager.register("color sensor", SensorMRColor.class);
+
+
+    }
+}
